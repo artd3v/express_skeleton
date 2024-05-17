2a9bfa5b637734ec3de2d58d133721a20ba95f40
artdev <artdevsemyonov@gmail.com>
2024-05-17 19:06:19 +0300

    add logs, add dependencies winston,app-root-path,nodemon

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var morgan = require('morgan');// var logger = require('morgan');
+var winston = require('./config/winston');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -13,7 +14,7 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
-app.use(morgan('combined')); // app.use(logger('dev'));
+app.use(morgan('combined', { stream: winston.stream })); // app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
@@ -27,15 +28,16 @@ app.use(function(req, res, next) {
   next(createError(404));
 });
 
-// Сделки с ошибками
+// error handler
 app.use(function(err, req, res, next) {
-  // Установка локально, предоставляя только ошибку в разработке
+  // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // рендеринг страницы ошибки
+  // add this line to include winston logging
+  winston.error(`${err.status || 500} - ${err.message} - ${req.originalUrl} - ${req.method} - ${req.ip}`);
+  // render the error page
   res.status(err.status || 500);
   res.render('error');
-});
+  });
 
 module.exports = app;
--- config/winston.js
+++ config/winston.js
@@ -1,2 +1,36 @@
 var appRoot = require('app-root-path');
 var winston = require('winston');
+// Определите пользовательские настройки для каждого транспорта (файл, консоль)(файл, консоль)
+var options = {
+    file: {
+        level: 'info',
+        filename: `${appRoot}/logs/app.log`,
+        handleExceptions: true,
+        json: true,
+        maxsize: 5242880, // 5MB
+        maxFiles: 5,
+        colorize: false,
+    },
+    console: {
+        level: 'debug',
+        handleExceptions: true,
+        json: false,
+        colorize: true,
+    },
+};
+// создать создание нового журнала Winston с настройками, определенными выше
+var logger = new winston.Logger({
+    transports: [
+        new winston.transports.File(options.file),
+        new winston.transports.Console(options.console)
+    ],
+    exitOnError: false, // Не выходите на обработанные исключения
+});
+// Создайте объект потока с функцией «записать», которая будет использоваться «Morgan»
+logger.stream = {
+    write: function(message, encoding) {
+// Используйте уровень журнала «Информация», чтобы выходной вывод был поднят обоим транспортом (файл и консоль)
+    logger.info(message);
+},
+};
+module.exports = logger;
